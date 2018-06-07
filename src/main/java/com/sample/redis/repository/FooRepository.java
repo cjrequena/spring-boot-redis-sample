204828ba9889325a983d75a57ee0ec6048274526
Carlos Requena <carlosjose.requena@gmail.com>
2018-06-07 13:03:50 +0200

    Added support fot consul

--- src/main/java/com/sample/redis/repository/FooRepository.java
+++ src/main/java/com/sample/redis/repository/FooRepository.java
@@ -4,6 +4,17 @@ import com.sample.redis.entity.FooEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ *
+ * @author cjrequena
+ * @version 1.0
+ * @see
+ * @since JDK1.8
+ */
 @Repository
 public interface FooRepository extends CrudRepository<FooEntity, String> {
 
